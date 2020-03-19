7758a80d7df0e63445f71934dc6571e15ee444ff
Nick Down <nick@nickdown.com>
2020-03-19 11:15:19 -0400

    wip [skip-ci]

--- fixtures/texttest_fixture.php
+++ fixtures/texttest_fixture.php
@@ -5,34 +5,37 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use App\GildedRose;
 use App\Item;
 
-echo "OMGHAI!\n";
+function tacos()
+{
+    file_put_contents("output.txt", "OMGHAI!\n");
 
-$items = array(
-    new Item('+5 Dexterity Vest', 10, 20),
-    new Item('Aged Brie', 2, 0),
-    new Item('Elixir of the Mongoose', 5, 7),
-    new Item('Sulfuras, Hand of Ragnaros', 0, 80),
-    new Item('Sulfuras, Hand of Ragnaros', -1, 80),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
-    // this conjured item does not work properly yet
-    new Item('Conjured Mana Cake', 3, 6)
-);
+    $items = array(
+        new Item('+5 Dexterity Vest', 10, 20),
+        new Item('Aged Brie', 2, 0),
+        new Item('Elixir of the Mongoose', 5, 7),
+        new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+        new Item('Sulfuras, Hand of Ragnaros', -1, 80),
+        new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+        new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
+        new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
+        // this conjured item does not work properly yet
+        new Item('Conjured Mana Cake', 3, 6)
+    );
 
-$app = new GildedRose($items);
+    $app = new GildedRose($items);
 
-$days = 2;
-if (count($argv) > 1) {
-    $days = (int) $argv[1];
-}
-
-for ($i = 0; $i < $days; $i++) {
-    echo("-------- day $i --------\n");
-    echo("name, sellIn, quality\n");
-    foreach ($items as $item) {
-        echo $item . PHP_EOL;
+    $days = 2;
+    if (count($argv) > 1) {
+        $days = (int)$argv[1];
     }
-    echo PHP_EOL;
-    $app->updateQuality();
+
+    for ($i = 0; $i < $days; $i++) {
+        file_put_contents("output.txt", "-------- day $i --------\n", FILE_APPEND);
+        file_put_contents("output.txt", "name, sellIn, quality\n", FILE_APPEND);
+        foreach ($items as $item) {
+            file_put_contents("output.txt", $item . PHP_EOL, FILE_APPEND);
+        }
+        file_put_contents("output.txt", PHP_EOL, FILE_APPEND);
+        $app->updateQuality();
+    }
 }
--- test/GildedRoseTest.php
+++ test/GildedRoseTest.php
@@ -2,9 +2,15 @@
 
 namespace App;
 
+require __DIR__;
 use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase {
+    /** @test */
+    public function it_tests_snapshot()
+    {
+
+    }
     public function testFoo() {
         $items      = [new Item("foo", 0, 0)];
         $gildedRose = new GildedRose($items);
