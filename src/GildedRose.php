a5e6df8c22d9f150d8360aba5a71d04cde9556b4
Christian Griffin <c.griffin@vehikl.com>
2020-03-19 11:39:44 -0400

    use snapshot testing

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -35,11 +35,11 @@ final class GildedRose {
                     }
                 }
             }
-            
+
             if ($item->name != 'Sulfuras, Hand of Ragnaros') {
                 $item->sell_in = $item->sell_in - 1;
             }
-            
+
             if ($item->sell_in < 0) {
                 if ($item->name != 'Aged Brie') {
                     if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
--- test/GildedRoseTest.php
+++ test/GildedRoseTest.php
@@ -3,14 +3,19 @@
 namespace App;
 
 use PHPUnit\Framework\TestCase;
+use Spatie\Snapshots\MatchesSnapshots;
 
 require __DIR__ . '/../fixtures/texttest_fixture.php';
 
 class GildedRoseTest extends TestCase {
+
+    use MatchesSnapshots;
+
     /** @test */
     public function it_tests_snapshot()
     {
         tacos();
+        $this->assertMatchesFileSnapshot(__DIR__ . '/../fixtures/output.txt');
     }
 
     public function testFoo() {
