5bee41878001efbd8493bb8ce93b10045966c43f
Nick Down <nick@nickdown.com>
2020-03-19 11:57:44 -0400

    Extract individual update item methods

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -12,49 +12,222 @@ final class GildedRose {
 
     public function updateQuality() {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
+            if($item->name === "Aged Brie") {
+                $this->updateAgedBrie($item);
+                continue;
+            }
+
+            if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
+                $this->updateBackstagePass($item);
+                continue;
+            }
+
+            if ($item->name === 'Sulfuras, Hand of Ragnaros') {
+                $this->updateSulfuras($item);
+                continue;
+            }
+
+            $this->updateItem($item);
+        }
+    }
+
+    /**
+     * @param $item
+     */
+    protected function updateItem($item): void
+    {
+        if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if ($item->quality > 0) {
+                if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                    $item->quality = $item->quality - 1;
+                }
+            }
+        } else {
+            if ($item->quality < 50) {
+                $item->quality = $item->quality + 1;
+                if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+                    if ($item->sell_in < 11) {
+                        if ($item->quality < 50) {
+                            $item->quality = $item->quality + 1;
+                        }
                     }
+                    if ($item->sell_in < 6) {
+                        if ($item->quality < 50) {
+                            $item->quality = $item->quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            $item->sell_in = $item->sell_in - 1;
+        }
+
+        if ($item->sell_in < 0) {
+            if ($item->name != 'Aged Brie') {
+                if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                    if ($item->quality > 0) {
+                        if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                            $item->quality = $item->quality - 1;
+                        }
+                    }
+                } else {
+                    $item->quality = $item->quality - $item->quality;
                 }
             } else {
                 if ($item->quality < 50) {
                     $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
+                }
+            }
+        }
+    }
+
+    /**
+     * @param $item
+     */
+    protected function updateAgedBrie($item): void
+    {
+        if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if ($item->quality > 0) {
+                if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                    $item->quality = $item->quality - 1;
+                }
+            }
+        } else {
+            if ($item->quality < 50) {
+                $item->quality = $item->quality + 1;
+                if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+                    if ($item->sell_in < 11) {
+                        if ($item->quality < 50) {
+                            $item->quality = $item->quality + 1;
                         }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
+                    }
+                    if ($item->sell_in < 6) {
+                        if ($item->quality < 50) {
+                            $item->quality = $item->quality + 1;
                         }
                     }
                 }
             }
+        }
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sell_in = $item->sell_in - 1;
-            }
+        if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            $item->sell_in = $item->sell_in - 1;
+        }
 
-            if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
+        if ($item->sell_in < 0) {
+            if ($item->name != 'Aged Brie') {
+                if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                    if ($item->quality > 0) {
+                        if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                            $item->quality = $item->quality - 1;
                         }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
                     }
                 } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
+                    $item->quality = $item->quality - $item->quality;
+                }
+            } else {
+                if ($item->quality < 50) {
+                    $item->quality = $item->quality + 1;
+                }
+            }
+        }
+    }
+
+    private function updateBackstagePass($item)
+    {
+        if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if ($item->quality > 0) {
+                if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                    $item->quality = $item->quality - 1;
+                }
+            }
+        } else {
+            if ($item->quality < 50) {
+                $item->quality = $item->quality + 1;
+                if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+                    if ($item->sell_in < 11) {
+                        if ($item->quality < 50) {
+                            $item->quality = $item->quality + 1;
+                        }
                     }
+                    if ($item->sell_in < 6) {
+                        if ($item->quality < 50) {
+                            $item->quality = $item->quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            $item->sell_in = $item->sell_in - 1;
+        }
+
+        if ($item->sell_in < 0) {
+            if ($item->name != 'Aged Brie') {
+                if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                    if ($item->quality > 0) {
+                        if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                            $item->quality = $item->quality - 1;
+                        }
+                    }
+                } else {
+                    $item->quality = $item->quality - $item->quality;
+                }
+            } else {
+                if ($item->quality < 50) {
+                    $item->quality = $item->quality + 1;
+                }
+            }
+        }
+    }
+
+    private function updateSulfuras($item)
+    {
+        if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if ($item->quality > 0) {
+                if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                    $item->quality = $item->quality - 1;
+                }
+            }
+        } else {
+            if ($item->quality < 50) {
+                $item->quality = $item->quality + 1;
+                if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+                    if ($item->sell_in < 11) {
+                        if ($item->quality < 50) {
+                            $item->quality = $item->quality + 1;
+                        }
+                    }
+                    if ($item->sell_in < 6) {
+                        if ($item->quality < 50) {
+                            $item->quality = $item->quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            $item->sell_in = $item->sell_in - 1;
+        }
+
+        if ($item->sell_in < 0) {
+            if ($item->name != 'Aged Brie') {
+                if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                    if ($item->quality > 0) {
+                        if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                            $item->quality = $item->quality - 1;
+                        }
+                    }
+                } else {
+                    $item->quality = $item->quality - $item->quality;
+                }
+            } else {
+                if ($item->quality < 50) {
+                    $item->quality = $item->quality + 1;
                 }
             }
         }
